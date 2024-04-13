de12f444f795cd23cc67c89a4899be1107984dfc
Sandeep Sharma <sandeepsharma2183@gmail.com>
2024-04-13 23:44:10 +0530

    updated

---
+++ DAY_032/isomorphic_string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char, int> ms;
+        unordered_map<char,int> mt; 
+        int n = s.size();
+        for (int i = 0; i < n; ++i) {
+            if (ms[s[i]] != mt[t[i]]) return false;
+            ms[s[i]] = i + 1;
+            mt[t[i]] = i + 1;
+        }
+        return true;
+    }
+};// 
+
+// i+1 vs i++;
